ba280c4c91bcab59745a52b3e92fcbc65513d712
Marcus Nguyen <thiennhank9@gmail.com>
2023-03-29 18:07:21 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import Prices from './features/prices';
-import './App.css';
+import React from "react";
+import Prices from "./features/prices";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
+      Test
       <Prices />
     </div>
   );
